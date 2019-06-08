772368420775dbfb3496e54da661118410a14967
chloewhen117 <chloewhen21@gmail.com>
2019-06-08 00:56:29 -0700

    Started template for landing page

---
+++ client/src/components/layouts/Landing.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react'
+
+class Landing extends Component {  
+  render() {
+    return (
+      <div className='app'>
+        <div className='app-body'>
+          <main className='main'>
+            Hello World
+          </main>
+        </div>
+      </div>
+    )
+  }
+}
+
+export default Landing;
